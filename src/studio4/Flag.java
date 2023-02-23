efc2fea57081b59208402159cd25e9ebe49031d0
sebastianmonge <sebastianmonge@localhost>
2023-02-23 14:18:27 -0600

    Made an enderdragon flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,32 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
-	}
+		StdDraw.setPenColor(Color.PINK);
+		StdDraw.filledRectangle(0.5,0.8, 0.5, 0.8);
+		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledRectangle(0.5, 0.5, 0.25, 0.15);
+		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledRectangle(0.35, 0.75, 0.1, 0.1);
+		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledRectangle(0.25, 0.7, 0.0825, 0.05);
+		
+		StdDraw.setPenColor(Color.MAGENTA);
+		StdDraw.setPenRadius(.04);
+		StdDraw.point(0.30, 0.8);
+		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledRectangle(0.30, 0.875, 0.02, 0.065);
+		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledRectangle(0.40, 0.875, 0.02, 0.065);
+		
+		double[] x = new double[] {.5, .525, .85};
+		double[] y = new double[] {.5, .8, .75}; 
+		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledPolygon(x, y);
+		}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,67 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
-	}
+		//System.out.println("What is the shape type?: ");
+		String shapeType = in.next();
+		
+		//System.out.println("What is the red component?: ");
+		int redComponent = in.nextInt();
+		
+		//System.out.println("What is the green component?: ");
+		int greenComponent = in.nextInt();
+		
+		//System.out.println("What is the blue component?: ");
+		int blueComponent = in.nextInt();
+		
+		//System.out.println("Is it filled?: ");
+		boolean isFilled = in.nextBoolean();
+		
+		//System.out.println("What is the parameter one?: ");
+		double parameterOne = in.nextDouble();
+		
+		//System.out.println("What is the parameter two?: ");
+		double parameterTwo = in.nextDouble();
+		
+		//System.out.println("What is the parameter three?: ");
+		double parameterThree = in.nextDouble();
+		
+		//System.out.println("What is the parameter four?: ");
+		double parameterFour = in.nextDouble();
+		
+		//System.out.println("What is the parameter four?: ");
+		double parameterFive = in.nextDouble();
+		
+		//System.out.println("What is the parameter four?: ");
+		double parameterSix = in.nextDouble();
+		
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+		
+		double[] x = new double[] {parameterOne, parameterThree, parameterFive};// array for polygon x values
+		double[] y = new double[] {parameterTwo, parameterFour, parameterSix}; //array for polygon y values
+		
+		if(shapeType.equals("rectangle")) { //right .equals since it is an object and not a primitive type
+			if(isFilled) {
+				StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+			else {
+				StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}
+		if(shapeType.equals("ellipse")) {
+			if(isFilled) {
+				StdDraw.filledEllipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+			else {
+				StdDraw.ellipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}
+		if(shapeType.equals("triangle")) { //triangle is polygon in StdDraw
+			if(isFilled) {
+				StdDraw.filledPolygon(x, y);
+			}
+			else {
+				StdDraw.polygon(x, y);
+			}
+		}
+	} 
 }
